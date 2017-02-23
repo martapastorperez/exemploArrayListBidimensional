2344378f9a6bdaa6662ceb422dd8b33529ffc77b
mpastorperez <mpastorperez@victoria31.danielcastelao.org>
2017-02-23 10:31:09 +0100

    0.1

--- src/exemploarraylistbi/ExemploArrayListBi.java
+++ src/exemploarraylistbi/ExemploArrayListBi.java
@@ -8,6 +8,8 @@ public class ExemploArrayListBi {
        li.crearLiga();
        li.amosar();
        li.buscarXogador();
+       li.darDeBaixa();
+       li.amosar();
         
     }
     
--- src/exemploarraylistbi/Liga.java
+++ src/exemploarraylistbi/Liga.java
@@ -1,68 +1,89 @@
-
-
 package exemploarraylistbi;
+
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class Liga {
-   
+
     int numEquipos = Integer.parseInt(JOptionPane.showInputDialog("cantos equipos :"));
-  
-    ArrayList<ArrayList<Xogador>>liga = new ArrayList<ArrayList<Xogador>>();
+
+    ArrayList<ArrayList<Xogador>> liga = new ArrayList<ArrayList<Xogador>>();
     ArrayList<Xogador> equipo;
-    
-    public void crearLiga(){
-        int i,j;
-    for( i=0;i<numEquipos;i++ ){
-      equipo = new ArrayList<Xogador>(); // creo fila vacia
-        
-      equipo=crearEquipo(); 
-      
-        liga.add(equipo);
-   }
-        
+    ArrayList<String> nombreArrayList = new ArrayList<String>();
+
+    public void crearLiga() {
+        int i, j;
+        for (i = 0; i < numEquipos; i++) {
+            equipo = new ArrayList<Xogador>(); // creo fila vacia
+
+            equipo = crearEquipo();
+
+            liga.add(equipo);
+        }
+
     }
-    
-    public void amosar(){
-        
-        for(int i =0;i< liga.size();i++){
+
+    public void amosar() {
+
+        for (int i = 0; i < liga.size(); i++) {
             System.out.println("\n");
-            for (int j=0;j<liga.get(i).size();j++){
+            for (int j = 0; j < liga.get(i).size(); j++) {
                 System.out.print(liga.get(i).get(j));
             }
+        }
     }
-    }    
-     public void buscarXogador(){
-         boolean atopado = false ; // non se atopou
-         System.out.println(" \n dorsal xogador a buscar :");
-         int dorsal = pedirInt();
-          for(int i =0;i< liga.size();i++)
-            
-            for (int j=0;j<liga.get(i).size();j++){
-                if(dorsal == liga.get(i).get(j).getDorsal()){
+
+    public void buscarXogador() {
+        boolean atopado = false; // non se atopou
+        System.out.println(" \n dorsal xogador a buscar :");
+        int dorsal = pedirInt();
+        for (int i = 0; i < liga.size(); i++) {
+            for (int j = 0; j < liga.get(i).size(); j++) {
+                if (dorsal == liga.get(i).get(j).getDorsal()) {
                     System.out.println(liga.get(i).get(j));
                     atopado = true;
                 }
-          } 
-         if (!atopado) // é igual que dicir o contrario de true
-             System.out.println(" o dorsal "+ dorsal + " non está na liga");
-      }
-      public static String pedirString(){
-        return (JOptionPane.showInputDialog(" dato String :") ); 
+            }
+        }
+        if (!atopado) // é igual que dicir o contrario de true
+        {
+            System.out.println(" o dorsal " + dorsal + " non está na liga");
+        }
     }
-    
-    public static int pedirInt(){
-        return(Integer.parseInt(JOptionPane.showInputDialog("dato int :")));
+
+    public static String pedirString() {
+        return (JOptionPane.showInputDialog(" dato String :"));
     }
-   
-     public ArrayList<Xogador> crearEquipo(){
+
+    public static int pedirInt() {
+        return (Integer.parseInt(JOptionPane.showInputDialog("dato int :")));
+    }
+
+    public ArrayList<Xogador> crearEquipo() {
         int tam = Integer.parseInt(JOptionPane.showInputDialog(" cantos xogadores :"));
-        for(int i=0;i< tam;i++){
-            equipo.add(new Xogador(pedirString(),pedirInt())); 
-            
-       
+        for (int i = 0; i < tam; i++) {
+            equipo.add(new Xogador(pedirString(), pedirInt()));
+
+        }
+        return equipo;
+
     }
-   return equipo;
-    
-}
+
+    public void darDeBaixa() {
+        System.out.println(" \n Dar de baixa:  ");
+        String nome = pedirString();
+        /*liga.remove(nome);*/
+
+        int dorsal = pedirInt();
+        for (int i = 0; i < liga.size(); i++) {
+            for (int j = 0; j < liga.get(i).size(); j++) {
+                if (dorsal == liga.get(i).get(j).getDorsal() && nome.equals(liga.get(i).get(j).getNome())) {
+                    liga.remove(liga.get(i).get(j));
+                    liga.get(i).remove(j);
+
+                }
+
+            }
+        }
+    }
 }
